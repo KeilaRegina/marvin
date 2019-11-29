be28df345f0dda2c8820fbd8e659d76286e81a09
KeilaRegina <57539471+KeilaRegina@users.noreply.github.com>
2019-11-29 14:03:33 -0300

    MAIúsculo

--- fase02/ex03/maiusculo.js
+++ fase02/ex03/maiusculo.js
@@ -1,6 +1,4 @@
 function maiusculo(palavra){
-  n=palavra.toUpperCase()
-    return n.substring(0,3) + "a" ;
-    return n.substring(0,3) + palavra.substring(3) ;
+ 
+    return palavra.toUpperCase().substring(0,3)  +palavra.substring(3) ;
 }
-  
